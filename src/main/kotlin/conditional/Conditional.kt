c63897cb57783873624fc41f4d82147fcbb43c71
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-27 08:46:09 +0530

    Copied the content of condition2() method to condition3() method

--- src/main/kotlin/conditional/Conditional.kt
+++ src/main/kotlin/conditional/Conditional.kt
@@ -28,6 +28,18 @@ class Conditional {
     }
 
     fun condition3(){
+        println("Enter your age:")
+        val age =  readLine()!!.toInt()
+        println("Age is: $age")
 
+        val message : String = if(age < 18){
+            "You are too young"
+        }else if (age == 100){
+            "Congratulations"
+        }else {
+            "You can vote"
+        }
+
+        println(message)
     }
 }
