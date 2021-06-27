66592a74e68cf0a15204adfd5f18ac97e09cd074
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-27 09:14:10 +0530

    Updated Player.kt class constructor to accept score variable.

--- src/main/kotlin/classes/Player.kt
+++ src/main/kotlin/classes/Player.kt
@@ -1,8 +1,6 @@
 package classes
 
-class Player(val name: String, var lives: Int = 3, var level: Int = 1) {
-    var score = 0
-
+class Player(val name: String, var lives: Int = 3, var level: Int = 1, var score: Int = 0) {
     fun show(){
         println("""
             Name: $name
