4ce81a7b793874f473e5a04a5dadec8c1b3e9943
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-27 08:23:04 +0530

    Added else statement

--- src/main/kotlin/conditional/Conditional.kt
+++ src/main/kotlin/conditional/Conditional.kt
@@ -2,9 +2,11 @@ package conditional
 
 class Conditional {
     fun condition(){
-        val lives = 3;
+        val lives = 3
         if(lives < 1){
             println("Game Over.")
+        }else {
+            println("You are still alive.")
         }
     }
 }
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 import conditional.Conditional
 
 fun main(args: Array<String>) {
-    val conditional : Conditional = Conditional();
-    conditional.condition();
+    val conditional : Conditional = Conditional()
+    conditional.condition()
 }
 
