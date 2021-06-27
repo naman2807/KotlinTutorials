c56ccf2f25de34b544b17378707cd6125a281482
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-27 09:13:35 +0530

    Updated Player.kt class constructor to accept level variable.

--- src/main/kotlin/classes/Player.kt
+++ src/main/kotlin/classes/Player.kt
@@ -1,7 +1,6 @@
 package classes
 
-class Player(val name: String, var lives: Int = 3) {
-    var level = 1
+class Player(val name: String, var lives: Int = 3, var level: Int = 1) {
     var score = 0
 
     fun show(){
