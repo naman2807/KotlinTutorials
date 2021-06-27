907ad4d9f8b85b6b35b1fed28c8f44395dd612ff
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-27 08:24:45 +0530

    Changed the if condition

--- src/main/kotlin/conditional/Conditional.kt
+++ src/main/kotlin/conditional/Conditional.kt
@@ -4,7 +4,8 @@ class Conditional {
     fun condition(){
         val lives = 3
         val isAlive = lives < 1
-        if(lives < 1){
+
+        if(isAlive){
             println("Game Over.")
         }else {
             println("You are still alive.")
