8186237dcf45790740cc43b9ed7e6ec20120a42e
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-27 09:49:07 +0530

    Added weapon damage to be printed in show() function

--- src/main/kotlin/classes/Player.kt
+++ src/main/kotlin/classes/Player.kt
@@ -10,7 +10,7 @@ class Player(val name: String, var lives: Int = 3, var level: Int = 1, var score
             Level: $level
             Score: $score
             Weapon Name: ${weapon.name}
-            
+            Weapon Damage Inflicted: ${weapon.damageInflicted}
         """.trimIndent())
     }
 }
