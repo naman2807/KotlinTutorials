97be7f0176759ae5924737a58f2736270aff800c
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-27 08:24:22 +0530

    Declared local boolean value isAlive

--- src/main/kotlin/conditional/Conditional.kt
+++ src/main/kotlin/conditional/Conditional.kt
@@ -3,6 +3,7 @@ package conditional
 class Conditional {
     fun condition(){
         val lives = 3
+        val isAlive = lives < 1
         if(lives < 1){
             println("Game Over.")
         }else {
