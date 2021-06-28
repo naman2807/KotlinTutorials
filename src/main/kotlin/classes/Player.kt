d8b1c9169ba166ff70806d8d109d775570ecf00d
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-28 10:03:51 +0530

    Check if lives > 0

--- src/main/kotlin/classes/Player.kt
+++ src/main/kotlin/classes/Player.kt
@@ -8,14 +8,9 @@ class Player(val name: String, var lives: Int = 3, var level: Int = 1, var score
     val inventory = ArrayList<Loot>()
 
     fun show(){
-        println("""
-            Name: $name
-            Lives: $lives
-            Level: $level
-            Score: $score
-            Weapon Name: ${weapon.name}
-            Weapon Damage Inflicted: ${weapon.damageInflicted}
-        """.trimIndent())
+        if(lives > 0){
+
+        }
     }
 
     override fun toString(): String {
