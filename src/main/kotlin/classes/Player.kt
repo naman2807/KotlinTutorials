7d382312c93a7262e526cfe82922f1decb8c6402
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-28 09:50:33 +0530

    Print the inventory item of player

--- src/main/kotlin/classes/Player.kt
+++ src/main/kotlin/classes/Player.kt
@@ -20,5 +20,6 @@ class Player(val name: String, var lives: Int = 3, var level: Int = 1, var score
 
     fun showInventory(){
         println("$name's inventory")
+        println(inventory.get(0))
     }
 }
