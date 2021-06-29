8b29d58906ad71964ea47a48c5e81e42ce2678b6
naman2807 <namanagarwaldbs438@gmail.com>
2021-06-29 12:08:37 +0530

    Created for loop from 1 to 10

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,36 +8,40 @@ fun main(args: Array<String>) {
 //    val conditional : Conditional = Conditional()
 //    conditional.condition2()
 
-    val naman = Player("Naman")
-    val harshit = Player("Harshit",5,1,2)
-
-//    println(naman.name)
-//    println(naman.lives)
-//    println(naman.level)
-//    println(naman.score)
-
-    naman.show()
-//    harshit.show()
-
-    harshit.weapon = naman.weapon
-    harshit.show()
-
-//    println(naman.weapon.name)
-//    println(naman.weapon.damageInflicted)
+//    val naman = Player("Naman")
+//    val harshit = Player("Harshit",5,1,2)
 //
-//    val axe = Weapon("Axe",12)
-//    naman.weapon = axe
-//    println(naman.weapon.name)
+////    println(naman.name)
+////    println(naman.lives)
+////    println(naman.level)
+////    println(naman.score)
+//
+//    naman.show()
+////    harshit.show()
+//
+//    harshit.weapon = naman.weapon
+//    harshit.show()
+//
+////    println(naman.weapon.name)
+////    println(naman.weapon.damageInflicted)
+////
+////    val axe = Weapon("Axe",12)
+////    naman.weapon = axe
+////    println(naman.weapon.name)
+//
+//    val redPotion = Loot("Red Potion", LootType.POTION, 7.50)
+//    val chestArmor = Loot("+3 Chest Armor", LootType.ARMOR, 80.0)
+//    naman.inventory.add(redPotion)
+//    naman.inventory.add(chestArmor)
+//    naman.showInventory()
+//
+//    naman.inventory.add(Loot("Ring of Protection +2", LootType.RING,52.0))
+//    naman.inventory.add(Loot("Invisibilty Potion", LootType.POTION, 21.0))
+//    naman.showInventory()
+//    println(naman)
 
-    val redPotion = Loot("Red Potion", LootType.POTION, 7.50)
-    val chestArmor = Loot("+3 Chest Armor", LootType.ARMOR, 80.0)
-    naman.inventory.add(redPotion)
-    naman.inventory.add(chestArmor)
-    naman.showInventory()
+    for (i in 1..10){
 
-    naman.inventory.add(Loot("Ring of Protection +2", LootType.RING,52.0))
-    naman.inventory.add(Loot("Invisibilty Potion", LootType.POTION, 21.0))
-    naman.showInventory()
-    println(naman)
+    }
 }
 
