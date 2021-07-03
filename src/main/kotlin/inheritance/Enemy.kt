cf531c386605099ae00038fa2fb786a04d29f9c5
naman2807 <namanagarwaldbs438@gmail.com>
2021-07-03 10:20:59 +0530

    Check if lives > 0

--- src/main/kotlin/inheritance/Enemy.kt
+++ src/main/kotlin/inheritance/Enemy.kt
@@ -16,6 +16,9 @@ open class Enemy(val name: String, var hitPoints: Int, var lives: Int) {
             println("$name took $damage points of damage, and has $hitPoints left.")
         }else{
             lives -= 1
+            if (lives > 0){
+
+            }
             println("$name lost a life")
         }
     }
